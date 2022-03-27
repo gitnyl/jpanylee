87d905e58fbc34bcd47780326da529d23f372596
nylee <nylee226@gmail.com>
2022-03-27 11:31:11 +0900

    init

---
+++ src/test/java/com/jpanylee/jpanylee/JpanyleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpanylee.jpanylee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpanyleeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
